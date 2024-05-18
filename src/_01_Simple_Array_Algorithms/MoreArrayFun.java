2c9190c98aef353736c678e52c5de2a5a1e98656
league <karinabriellen@gmail.com>
2024-05-17 18:27:24 -0700

    do the 2d

--- src/_01_Simple_Array_Algorithms/MoreArrayFun.java
+++ src/_01_Simple_Array_Algorithms/MoreArrayFun.java
@@ -5,20 +5,20 @@ import java.util.Random;
 public class MoreArrayFun {
     //1. Create a main method to test the other methods you write.
 public static void main(String[] args) {
-	String[] words = new String[5];
-	words[0] = "hello";
-	words[1] = "cup";
-	words[2] = "jacket";
-	words[3] = "blue";
-	words[4] = "square";
-	printAllStringsInArray(words);
+//	String[] words = new String[5];
+//	words[0] = "hello";
+//	words[1] = "cup";
+//	words[2] = "jacket";
+//	words[3] = "blue";
+//	words[4] = "square";
+//	printAllStringsInArray(words);
 	String[] hello = new String[5];
 	hello[0] = "hello";
 	hello[1] = "cup";
 	hello[2] = "jacket";
 	hello[3] = "blue";
 	hello[4] = "square";
-	printAllStringsInArrayBackwards(hello);
+	printAllOfTheStringsInARandomOrder(hello);
 	
 }
 	
@@ -52,7 +52,25 @@ public static void printEveryOtherStringInTheArray(String[] strings) {
     //5. Write a method that takes an array of Strings and prints all the Strings in the array
     //   in a completely random order. Almost every run of the program should result in a different order.
 public static void printAllOfTheStringsInARandomOrder(String[] values) {
+	Random rand = new Random();
+	while(arrayHasOneNonNull(values)) {
+		int number = rand.nextInt(values.length);
+		if(values[number] != null) {
+			System.out.println(values[number]);
+			values[number] = null;
+		}
+	}
 	
+	
+	
+}
+public static boolean arrayHasOneNonNull(String[] strings) {
+	for(int i = 0; i < strings.length; i++) {
+		if(strings[i] != null) {
+			return true;
+		}
+	}
+	return false;
 }
 }
 
